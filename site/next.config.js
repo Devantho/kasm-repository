b95e00281cf5c0e2f5d638c9cda629bed83066c3
Devantho <116366449+Devantho@users.noreply.github.com>
2023-09-09 10:25:38 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Beast_of_mine Repo',
+    description: 'My repo',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
